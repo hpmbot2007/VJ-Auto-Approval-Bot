8634ec532401a8ece2a38f598f22bb797ca9310a
hpmbot2007 <150565901+hpmbot2007@users.noreply.github.com>
2023-11-12 21:23:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,7 +11,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "6323337513:AAHOmEGnYfBAadhq1h1eVH8CrTRI9Tfrl2g")
     FSUB = getenv("FSUB", "WMchannelS")
     CHID = int(getenv("CHID", "-1001953506128"))
-    SUDO = list(map(int, getenv("SUDO", "1918917066").split()))
+    SUDO = list(map(int, getenv("SUDO", "1918917066 1512795317").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://SERIAL:SERIAL@cluster0.m6omsjh.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
